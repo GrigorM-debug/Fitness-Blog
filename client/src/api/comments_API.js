de652bfcba3ee3518a46abb4737711be7a2419ae
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-07-30 23:44:06 +0300

    Added comments API

---
+++ client/src/api/comments_API.js
@@ -0,0 +1,24 @@
+const BASE_URL = 'http://localhost:3030/data/comments';
+
+export default async function createComment(gameId, text) {
+    const token = localStorage.getItem('auth-token');
+
+    await fetch(BASE_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Authorization': token
+        },
+        body: JSON.stringify({gameId, text})
+    })
+}
+
+export default async function getAll(gameId) {
+    const params = new URLSearchParams({
+        where: `gameId="${gameId}"`
+    })
+
+    const response = await fetch(`${BASE_URL}/?${params.toString()}`);
+    const comments = response.json()
+    return comments;
+}
