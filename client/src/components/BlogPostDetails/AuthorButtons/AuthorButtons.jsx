d2730023c0f38bce0954d20a9b6b4c41db1b40e1
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-08-03 23:49:20 +0300

    Change the AuthButtons in BlogDetails from Links to Buttons

--- client/src/components/BlogPostDetails/AuthorButtons/AuthorButtons.jsx
+++ client/src/components/BlogPostDetails/AuthorButtons/AuthorButtons.jsx
@@ -1,19 +1,18 @@
 import styles from './AuthorButtons.module.css';
-import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 export default function AuthorButtons() {
     return (
         <div className={styles.authButtons}>
-            <Link to="#">
+            <button type='submit'>
                 Edit
                 <FontAwesomeIcon icon={faEdit} style={{color: "#ffffff", marginLeft: '0.5em'} } />
-            </Link>
-            <Link to="#">
+            </button>
+            <button type='submit'>
                 Delete
                 <FontAwesomeIcon icon={faTrash} style={{marginLeft: '0.5em'}}/>
-            </Link>
+            </button>
         </div>
     );
 };
